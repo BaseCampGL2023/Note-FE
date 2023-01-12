dee4e729df6a4ba8af7a1cad722a9ef1f2fcbcce
valerii nikitin <nikitinrus404@gmail.com>
2023-01-12 21:45:07 +0200

    updated header;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
+import { MainComponent } from './main/main.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [AppComponent, HeaderComponent, MainComponent, NoteComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
     providers: [],
     bootstrap: [AppComponent],
