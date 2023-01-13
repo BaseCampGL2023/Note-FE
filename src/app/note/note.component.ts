b77682ca8c88799e0f84a11b97adc972331c4cc3
valerii nikitin <nikitinrus404@gmail.com>
2023-01-13 17:29:35 +0200

    added main content

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { Note } from '../Models/Note';
+import { Tag } from '../Models/Tag';
+import { ImageInNote } from '../Models/Image';
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
+  constructor() {
+    this.note = new Note(1, 1, "This is a note.", new Date(), new Date(), [new Tag(1, "Tag1"), new Tag(2, "Tag2")], new ImageInNote(1, 1, 'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'));
 
+  }
+  public note: Note;
 }
